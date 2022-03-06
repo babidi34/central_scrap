a4d5a61c07f676e57d0d35cc37e4350e541ca13f
babidi34 <karimbaidi@gmail.com>
2022-03-06 13:24:13 +0100

    run function dynamically

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import config
 import fonctions
+import sys
 
-data_paca = fonctions.scrap_offre_alimentaire_paca()
-fonctions.createCsv(data_paca)
+# fonction dynamically
+name_fonction = "scrap_"+config.nom_site.replace("-","_")
+function_current = getattr(fonctions,name_fonction)
+
+data_list = function_current()
+print(data_list)
